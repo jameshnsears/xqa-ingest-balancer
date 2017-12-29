42b60330fa0a4a01e78ff4a27ede4c9c17856a5e
James Sears <james.hn.sears@gmail.com>
2017-12-29 17:32:09 +0000

    attempt to improve utf-8 support

--- src/main/java/xqa/commons/MessageLogging.java
+++ src/main/java/xqa/commons/MessageLogging.java
@@ -4,6 +4,8 @@ import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.qpid.jms.message.JmsBytesMessage;
 import org.apache.qpid.jms.message.JmsObjectMessage;
 import org.apache.qpid.jms.message.JmsTextMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.jms.Destination;
 import javax.jms.JMSException;
@@ -13,6 +15,8 @@ import java.io.UnsupportedEncodingException;
 import java.text.MessageFormat;
 
 public class MessageLogging {
+    private static final Logger logger = LoggerFactory.getLogger(MessageLogging.class);
+    
     public static String log(Direction direction, Message message, boolean useDigest) throws JMSException, UnsupportedEncodingException {
         String arrowDirection = getArrow(direction);
         String jmsTimestamp = Long.toString(message.getJMSTimestamp());
@@ -27,6 +31,7 @@ public class MessageLogging {
 
     public static String getTextFromMessage(Message message) throws JMSException, UnsupportedEncodingException {
         if (message instanceof JmsBytesMessage) {
+            logger.debug("JmsBytesMessage");
             JmsBytesMessage jmsBytesMessage = (JmsBytesMessage) message;
             jmsBytesMessage.reset();
             byte[] byteData;
@@ -34,12 +39,14 @@ public class MessageLogging {
             jmsBytesMessage.readBytes(byteData);
             return new String(byteData, "UTF-8");
         } else if (message instanceof JmsObjectMessage) {
+            logger.debug("JmsObjectMessage");
             JmsObjectMessage jmsObjectMessage = (JmsObjectMessage) message;
             Serializable serializable = jmsObjectMessage.getObject();
-            return serializable.toString();
+            return new String(serializable.toString().getBytes(),"UTF-8");
         } else {
+            logger.debug("JmsTextMessage"); 
             JmsTextMessage jmsTextMessage = (JmsTextMessage) message;
-            return jmsTextMessage.getText();
+            return new String(jmsTextMessage.getText().getBytes(),"UTF-8");
         }
     }
 
